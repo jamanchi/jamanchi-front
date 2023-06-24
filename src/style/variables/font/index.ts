b0db898d3060865091cb1b1a491899ee7b3d884b
merge <fpel8800@gmail.com>
2023-06-25 02:59:22 +0900

    Feat: font 전역 style 추가

---
+++ src/style/variables/font/index.ts
@@ -0,0 +1,12 @@
+import { css } from '@emotion/react';
+
+export const font = {
+  title: css`
+    font-size: 35px;
+    font-weight: bold;
+  `,
+  subTitle: css`
+    font-size: 20px;
+    font-weight: bold;
+  `,
+};
