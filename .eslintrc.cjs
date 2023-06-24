2dae60544cb407866bbe24bba281e62249fa8127
merge <fpel8800@gmail.com>
2023-06-24 14:34:47 +0900

    Chore: prettier 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,11 @@
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    "airbnb",
-    "airbnb/hooks"
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'airbnb', 'airbnb/hooks'],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
-}
+};
