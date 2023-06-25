5fcb6e4bfd2821c2b3f4480cc0b55581a590995e
merge <fpel8800@gmail.com>
2023-06-26 02:20:48 +0900

    Feat: get hobbies API 커스텀 훅 추가

---
+++ src/pages/hobby/hooks/useFetchHobbies.ts
@@ -0,0 +1,13 @@
+import { useInfiniteQuery } from 'react-query';
+
+const fetchHobbies = async () =>
+  await (await fetch(`/api/v1/hobbies/sub`)).json();
+
+const useFetchHobbies = () =>
+  useInfiniteQuery({
+    queryKey: ['hobbies'],
+    queryFn: fetchHobbies,
+    getNextPageParam: (_, allPages) => allPages.length + 1,
+  });
+
+export default useFetchHobbies;
