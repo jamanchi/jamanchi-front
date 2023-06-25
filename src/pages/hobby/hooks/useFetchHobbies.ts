8b2639b7b08dabd3e03b5f5a2c700f23b57db083
merge <fpel8800@gmail.com>
2023-06-26 03:14:07 +0900

    Fix: react import 수정

--- src/pages/hobby/hooks/useFetchHobbies.ts
+++ src/pages/hobby/hooks/useFetchHobbies.ts
@@ -1,4 +1,4 @@
-import { useInfiniteQuery } from 'react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
 const fetchHobbies = async () =>
   await (await fetch(`/api/v1/hobbies/sub`)).json();
