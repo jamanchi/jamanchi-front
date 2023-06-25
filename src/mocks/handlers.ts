0b02e4c06d1c1b41d0c7e44e494e945384647b94
tnghgks <tnghgks@naver.com>
2023-06-26 01:10:33 +0900

    Feat: Mock Api 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,21 +4,34 @@ let count = 0;
 
 export const handlers = [
   // 답변결과조회
-  rest.get('/api/v1/answer', (_, res, ctx) =>
-    res(
-      ctx.status(200),
-      ctx.json({
-        cost: {
-          price: 10000,
-          avgPrice: 30000,
-        },
-        description: '스키에 대한 설명입니다.',
-        items: {
-          item: '물건과 가격정보',
-        },
-      })
-    )
-  ),
+  rest.get('/api/v1/answer', (req, res, ctx) => {
+    const hobbyId = req.url.searchParams.get('hobbyId');
+    const keywordId1 = req.url.searchParams.get('keywordId1');
+    const keywordId2 = req.url.searchParams.get('keywordId2');
+    if (hobbyId && keywordId1 && keywordId2) {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          id: hobbyId,
+          title: '스키',
+          image:
+            'https://media.istockphoto.com/id/1270325495/ko/%EC%82%AC%EC%A7%84/%EA%B7%B8%EB%A3%B9-%EC%8A%A4%ED%82%A4.jpg?s=2048x2048&w=is&k=20&c=lqEZTdJtO5Mai-4an6SaGEK58WC30wD_DZf0gxjOvNA=',
+          result: [
+            {
+              keyword: keywordId1,
+              description: '스키 입니다.',
+            },
+            {
+              keyword: keywordId2,
+              description: '이용 금액은 30000원입니다.',
+            },
+          ],
+        })
+      );
+    }
+
+    return res(ctx.status(400));
+  }),
 
   // 서비스 이용횟수조회 (get)
   rest.get('/api/v1/summary', (_, res, ctx) =>
