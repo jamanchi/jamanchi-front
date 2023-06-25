932c9f95976bf82917bd53dd794413c85cf8a943
tnghgks <tnghgks@naver.com>
2023-06-26 04:23:29 +0900

    Fix: 프록시 추가로 CORS Error 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  server: {
+    proxy: {
+      '/api':
+        'https://port-0-jamanchi-back-7xwyjq992llj9hm2l9.sel4.cloudtype.app',
+    },
   },
 });
