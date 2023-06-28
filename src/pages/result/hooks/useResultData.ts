0f4aa8dbedf1bfd7ae723edd5ec0b33ebfd24934
tnghgks <tnghgks@naver.com>
2023-06-28 23:54:17 +0900

    Feat: useResultData 함수 타입지정

--- src/pages/result/hooks/useResultData.ts
+++ src/pages/result/hooks/useResultData.ts
@@ -1,7 +1,24 @@
 import { useQueries } from '@tanstack/react-query';
 import { PROXY } from '@/constants/proxyURL';
 
-const getResult = async (hobbyId: string, keywords: string[]) => {
+interface IResult {
+  title: string;
+  image: string;
+  result: IResultItem[];
+}
+
+interface IResultItem {
+  keyword: string;
+  description: string;
+}
+interface IHobbyData extends IResultItem {
+  image: string;
+}
+
+const getResult = async (
+  hobbyId: string,
+  keywords: string[]
+): Promise<IResult> => {
   const data = await (
     await fetch(
       `${PROXY}/api/v1/answer?hobbyId=${hobbyId}&keywordId1=${keywords[0]}&keywordId2=${keywords[1]}`
@@ -10,7 +27,7 @@ const getResult = async (hobbyId: string, keywords: string[]) => {
 
   return data;
 };
-const getImage = async (hobbyId: string) => {
+const getImage = async (hobbyId: string): Promise<IHobbyData> => {
   const data = await (await fetch(`${PROXY}/api/v1/hobbies/${hobbyId}`)).json();
 
   return data;
@@ -20,17 +37,20 @@ const useResultData = (hobbyId: string, keywords: string[]) => {
   const res = useQueries({
     queries: [
       {
-        queryKey: ['resultData'],
+        queryKey: ['resultData', hobbyId],
         queryFn: () => getResult(hobbyId, keywords),
       },
       {
-        queryKey: ['imageData'],
+        queryKey: ['imageData', hobbyId],
         queryFn: () => getImage(hobbyId),
       },
     ],
   });
 
-  return [res[0].data, res[1].data];
+  return {
+    resultData: res[0].data,
+    hobby: res[1].data,
+  };
 };
 
 export default useResultData;
